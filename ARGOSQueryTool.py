8e265d3b6ae6af81a614933c845a842681414441
lmd61 <42805191+lmd61@users.noreply.github.com>
2018-10-01 11:36:16 -0400

    Reads line from file

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -6,13 +6,20 @@
 # Created on: Sept, 2018
 # ARGOSQueryTool.py
 #
-# Description: Parses a line of ARGOS tracking data 
-#
-# Created by: John Fay (john.fay@duke.edu)
-# Created on: Sept, 2018
+# Create a variable pointing to the file with no header
+fileName = "SaraNoHeader.txt"
 
-# Copy and paste a line of data as the startLine variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+# Open the file as a read-only file object
+fileObj = open(fileName, 'r')
+
+# Read in all lines in the text file into a list variable
+lineList = fileObj.readlines()
+
+# Closes the file object (now that we have all we need)
+fileObj.close()
+
+# Extract the first line from the lineList
+lineString = lineList[0]
 
 # Use the split command to parse the items in lineString into a list object
 lineData = lineString.split("\t")
@@ -26,5 +33,5 @@ obsLC = lineData[3]                 # Observation Location Class
 obsLat = lineData[5]                # Observation Latitude
 obsLon = lineData[6]                # Observation Longitude
 
-# Print information to the use
+# Print information to the user
 print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
